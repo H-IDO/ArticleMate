a42e04ed4ef0f2938ed6e663bdbe57c884d1f21b
Ido Hoorvitch <ido@AetosDios.local>
2020-10-09 17:33:03 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArticleMate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
